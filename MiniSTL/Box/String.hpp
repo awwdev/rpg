dd7c63255627ea156f78ae91237a5b46df426571
awwdev <aw.mail.ranked@gmail.com>
2020-05-08 19:17:39 +0200

    more string impl

--- MiniSTL/Box/String.hpp
+++ MiniSTL/Box/String.hpp
@@ -1,61 +1,154 @@
+//https://github.com/awwdev
+
+/*
+## mini::box::String
+
+- bounds checking is toggleable via macro, no exceptions are used
+- overall more readable and extendible than the STL
+*/
+
 #pragma once
 #include "MiniSTL/Types.hpp"
 #include "MiniSTL/Debug/Logger.hpp"
 
+
 namespace mini::box
 {
-    template<typename CHAR_T>
+#define DO_BOUNDS_CHECK 1
+
+
+    //size agnostic in type - use to pass string around like IString&
+    template<typename CHAR_T = char>
     struct IString
     {
-        u32 count = 0;
+        using DATA_T = CHAR_T;
+        const u32 COUNT_MAX;
+
+
+        ///ACCESS
+
+        CHAR_T* const dataPtr = nullptr;
+        
+        inline auto&        operator[](const u32 i)       { CheckBounds(); return dataPtr[i]; }
+        inline auto& const  operator[](const u32 i) const { CheckBounds(); return dataPtr[i]; }
+
 
         ///MOD
 
-        void Clear()  { dataPtr[0] = '\0'; }
-        void Erase()  {}
+        void Clear() { dataPtr[0] = '\0'; count = 0; }
 
-        inline auto&        operator[](const u32 i)         { return dataPtr[i]; }
-        inline auto& const  operator[](const u32 i) const   { return dataPtr[i]; }
 
-        void operator=(const CHAR_T chars[])
+        ///SET
+
+        template<class CHAR_ARR>
+        constexpr void Set(const CHAR_ARR& str) //possibly called by ctor so constexpr
         {
-            for (u32 i = 0; ; ++i)
-            {
-                if ((dataPtr[i] = chars[i]) == '\0')
-                    break;
+            CheckBounds(count + sizeof(CHAR_ARR), COUNT_MAX + 1);
+            //memcpy and std::copy are not constexpr?
+            for (u32 i = 0; i < sizeof(CHAR_ARR); ++i) {
+                dataPtr[i] = str[i];
             }
+            count = sizeof(CHAR_ARR) - 1;
         }
 
-        void operator+(const CHAR_T chars[])
+
+        ///APPEND
+
+        template<class CHAR_ARR>
+        void AppendChars(const CHAR_ARR& chars)
         {
-
+            //CheckBounds();
+            std::memcpy(dataPtr + count, chars, sizeof(CHAR_ARR));
+            count += sizeof(CHAR_ARR) - 1;
         }
-        
+
+        void AppendString(const IString& str)
+        {
+            static_assert(false, "Not yet implemented");
+        }
+
+
+        ///REMOVE
+
+        void Remove()
+        {
+            static_assert(false, "Not yet implemented");
+        }
+
+
+        ///OTHER
+
+        bool Contains()
+        {
+            static_assert(false, "Not yet implemented");
+            return true;
+        }
+
+
+        IString(const IString& str)        { }
+        void operator=(const IString& str) { }
+
     protected:
-        IString(CHAR_T* const data) : dataPtr { data } { ; }
-        CHAR_T* dataPtr;
+        u32 count = 0; //does not include \0
+
+        ///CTOR
+
+        constexpr IString(CHAR_T* const data, const u32 countMax) : dataPtr{ data }, COUNT_MAX { countMax } { ; }
+        
+        ///INTERNAL
+
+        constexpr void CheckBounds(const u32 i, const u32 size) const
+        {
+        #if (DO_BOUNDS_CHECK)
+            if (i < 0 || i >= size)
+            {
+                mini::dbg::dlog<mini::dbg::ColorMode::Red>("String access out of bounds");
+                __debugbreak();
+            }
+        #endif
+        }
     };
 
+
     template<u32 COUNT_MAX_T, typename CHAR_T = char>
-    struct String : public IString<CHAR_T>
+    struct String final : IString<CHAR_T>
     {
         using DATA_T = CHAR_T;
+        using BASE   = IString<CHAR_T>;
+
         constexpr static u32 COUNT_MAX = COUNT_MAX_T;
 
         ///CTOR
 
-        String(const CHAR_T chars[] = "") : IString<CHAR_T>(data) { IString<CHAR_T>::operator=(chars); }
+        constexpr String() : BASE(data, COUNT_MAX) {;}
 
-        ///DATA ARRAY
+        template<class CHAR_ARR>
+        constexpr String(const CHAR_ARR& chars) : String() { BASE::Set(chars); }
 
-        CHAR_T data[COUNT_MAX_T] = {};
+        String(const String& str) : String() { IString(str); }
+        void operator=(const String& str)    { BASE::operator=(str); }
+
+    private:
+        CHAR_T data[COUNT_MAX] = { 0 };
     };
 
+
+    ///OTHER
 
     template<class... STRINGS>
     auto ConcatStrings(const STRINGS&... strs)
     {
-        return nullptr; //todo
+        static_assert("not yet implemented");
+    }
+
+
+    template<typename CHAR_T>
+    std::ostream& operator<<(std::ostream& os, const IString<CHAR_T>& str)
+    {
+        os << str.dataPtr;
+        return os;
     }
 
 }//ns
+
+#undef DO_BOUNDS_CHECK
