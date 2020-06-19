7bf207a5e88c532595e5a28fb743e35ecba77b85
awwdev <aw.mail.ranked@gmail.com>
2020-06-19 19:07:22 +0200

    StringMap

--- src/mini/Box/StringMap.hpp
+++ src/mini/Box/StringMap.hpp
@@ -6,17 +6,22 @@
 
 namespace mini::box
 {
-    template<class VALUE, u8 BUCKET_COUNT_T = 20, u8 BUCKET_LENGTH_T = 20>
+    constexpr bool AreStringsEqual(const char* a, const char* b)
+    {
+        while (*a && *a == *b) { ++a; ++b; }
+        return *a == *b;
+    }
+
+    template<class VALUE>
     struct StringMap
     {
-        constexpr static auto BUCKET_COUNT  = BUCKET_COUNT_T;
-        constexpr static auto BUCKET_LENGTH = BUCKET_LENGTH_T;
+        constexpr static auto BUCKET_COUNT    = 20;
+        constexpr static auto BUCKET_LENGTH   = 20;
+        constexpr static auto STRING_CAPACITY = 20;
 
         struct Pair 
         {
-            constexpr static auto STRING_CAPACITY = 20;
-
-            char str[STRING_CAPACITY]; //capacity
+            char   str[STRING_CAPACITY]; //key
             VALUE  val;
 
             constexpr Pair() = default;
@@ -41,10 +46,9 @@ namespace mini::box
 
         struct Bucket
         {
-            u8 count = 0;
-            Pair content[BUCKET_LENGTH];
+            u8   count = 0;
+            Pair content[BUCKET_LENGTH] {};
 
-            constexpr Bucket() : content {} {}
             constexpr void Add(const Pair& pair)
             {
                 content[count] = pair;
@@ -52,24 +56,37 @@ namespace mini::box
             }
         };
 
+
         constexpr u8 Hash(chars_t str) const
         {
             return (str[0] + (str[1] != '\0' ? str[1] : 0)) % BUCKET_COUNT;
         }
 
-
-        constexpr StringMap() : buckets {} {}
-        constexpr StringMap(std::initializer_list<Pair> pairs) : StringMap()
+        constexpr StringMap(std::initializer_list<Pair> pairs)
         {
-            for(const auto& pair : pairs)
-            {
+            for(const auto& pair : pairs) {
                 const auto hash = Hash(pair.str);
+                //! there is no uniqueness check 
                 buckets[hash].Add(pair);
             }
         }
 
+        constexpr const VALUE* GetOptional(chars_t str) const
+        {
+            const auto hash = Hash(str);
+            auto& bucket = buckets[hash];
+            for(u8 contentIdx = 0; contentIdx < bucket.count; ++contentIdx) {
+                if (AreStringsEqual(bucket.content[contentIdx].str, str))
+                    return &(bucket.content[contentIdx].val);
+            }
+            return nullptr;
+        }
+        //TODO: const version missing
+
+
         Bucket buckets[BUCKET_COUNT];
     };
+
 
     template<class VALUE>
     inline void PrintStringMap(const StringMap<VALUE>& map)
@@ -79,7 +96,7 @@ namespace mini::box
             const auto& bucket = map.buckets[bucketIdx];
             for(u8 contentIdx = 0; contentIdx < bucket.count; ++contentIdx)
             {
-                LOG((int)bucketIdx, bucket.content[contentIdx].val);
+                LOG((int)bucketIdx, bucket.content[contentIdx].val, bucket.content[contentIdx].str);
             }
         }
     }
