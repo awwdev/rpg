42c9ccc1dce8c84295c3ff2cbc263f5d346970ec
awwdev <aw.mail.ranked@gmail.com>
2020-07-07 00:23:47 +0200

    new vs color theme 2

--- src/main.cpp
+++ src/main.cpp
@@ -19,46 +19,45 @@
 using namespace mini;
 using namespace mini::wnd;
 
-
 int WINAPI wWinMain(
-    _In_        HINSTANCE   hInstance,
-    _In_opt_    HINSTANCE   hPrevInstance,
-    _In_        PWSTR       pCmdLine,
-    _In_        int         nCmdShow)
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ PWSTR pCmdLine,
+    _In_ int nCmdShow)
 {
     {
-        dbg::Console console {};
-        wnd::Window  window { hInstance, 800, 600 };
+        dbg::Console console{};
+        wnd::Window window{hInstance, 800, 600};
         mem::GlobalAllocate();
-        hostRes::HostResources hostResources {}; //will load immediately for now
+        hostRes::HostResources hostResources{}; //will load immediately for now
 
         constexpr auto s = sizeof vk::VkRenderer;
         auto ptrRenderer = mem::ClaimBlock<vk::VkRenderer>(
             vk::WindowHandle{window.hInstance, window.hWnd},
-            hostResources
-        );
-        
+            hostResources);
+
         auto ptrSceneStack = mem::ClaimBlock<box::Array<app::Scene, 1, box::INIT::Yes>>();
-        uint32_t sceneIdx  = 0;
+        uint32_t sceneIdx = 0;
 
         while (!wnd::CheckEvent(EventType::Window_Close) && !wnd::IsClicked(EventType::Keyboard_Escape))
         {
             wnd::PollEvents();
-            dt::UpdateFPS();     
+            dt::UpdateFPS();
 
-            if (wnd::window_h != 0 && wnd::window_w != 0) {
+            if (wnd::window_h != 0 && wnd::window_w != 0)
+            {
                 rendergraph::renderGraph.Clear();
                 ptrSceneStack[sceneIdx].Update(mini::dt::seconds);
                 ptrRenderer->Render(mini::dt::seconds, ptrSceneStack[sceneIdx]);
-            }   
+            }
         }
-        
+
         VK_CHECK(vkDeviceWaitIdle(vk::context.device));
     }
 
     mem::GlobalDeallocate();
     system("pause");
 
-}//main end
+} //main end
 
 //TODO: Resource Manager
