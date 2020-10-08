024b2cc4350911e83e91f3822ba90b67c2d72796
awwdev <aw.mail.ranked@gmail.com>
2020-10-08 12:25:50 +0200

    scene tree nesting works

--- src/gui/Editor/GUI_Scene.hpp
+++ src/gui/Editor/GUI_Scene.hpp
@@ -4,6 +4,7 @@
 #include "gui/Widgets/Widget_List.hpp"
 
 #include "ecs/ECS.hpp"
+#include <functional>
 
 namespace rpg::gui {
 
@@ -17,20 +18,31 @@ struct GUI_Scene
 
     Widget_List<ecs::ID> entityList
     {
-        .listName = "Entities"
-
+        .listName = "Entities",
+        .maxHeight = wnd.rect.height - wnd.BAR_H - 4,
     };
 
     void Update(gpu::RenderData& renderData, ecs::ECS& ecs)
     {
+        //? UPDATE SCENE TREE
+        //TODO: subscription model instead of hard update per frame
         entityList.topLevelItems.Clear();
-        FOR_ARRAY(ecs.entitiesUsed, i) {
-            auto const  entityID = ecs.entitiesUsed[i];
-            if (auto const nameComponent = ecs.arrays.nameComponents.GetOptional(entityID)){
-                auto* parent = entityList.AddItem(nameComponent->name.Data(), nameComponent->name.Length());
-            }
-        }
+        std::function<void(ecs::ID const, Widget_List<ecs::ID>::Item*)> addEntity = [&](ecs::ID const entityID, Widget_List<ecs::ID>::Item* parent){
+             if (auto* const nameComponent = ecs.arrays.nameComponents.GetOptional(entityID)){
+                 auto* currParent = entityList.AddItem(nameComponent->name.Data(), nameComponent->name.Length(), parent);
+                    if (auto* const mainComponent = ecs.arrays.mainComponents.GetOptional(entityID)){
+                    FOR_ARRAY(mainComponent->children, i){
+                        addEntity(mainComponent->children[i], currParent);
+                    }
+                }
+             }
+        };
+        FOR_ARRAY(ecs.entitiesUsed, i){
+            auto const entityID = ecs.entitiesUsed[i];
+            addEntity(entityID, nullptr);
+        }        
 
+        //? UPDATE
         wnd.Update(renderData);
         entityList.Update(renderData, wnd);
     }
